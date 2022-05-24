e88bf95e6c8618daf431e6282b0cea6fd5488dbc
giorgi-digmelashvili <giorgi.digmelashvili@hp.com>
2022-05-24 14:48:47 -0700

    initiate new RN windows app

---
+++ windows/drawernavigationsample/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::drawernavigationsample::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::drawernavigationsample::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
